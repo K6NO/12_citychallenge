aa372231b8d69b3440656d624dc952b66ea10de1
K6NO <tamas.kenessey@gmail.com>
2017-07-23 10:04:44 +0200

    remove secret.json

--- src/config/passport.js
+++ src/config/passport.js
@@ -6,8 +6,6 @@ const
     User = require('../models/user').User;
 
 // Access credentials
-//const secret = require('./secret.json');
-
 let facebookClientId = process.env.FACEBOOK_CLIENT_ID; //|| secret.FACEBOOK_CLIENT_ID;
 let facebookClientSecret = process.env.FACEBOOK_CLIENT_SECRET; //|| secret.FACEBOOK_CLIENT_SECRET;
 let googleClientId = process.env.GOOGLE_CLIENT_ID; // || secret.GOOGLE_CLIENT_ID;
